7e7aa0b662ac8a1ad4c9c2a38c4df64062711697
imane-aanna <aannaque.imane@gmail.com>
2016-04-28 17:23:58 +0200

    Added function PropertyNames()
    This function displays all the properties we have.
    I am not sure if the result should be a list. To be confirmed by @pradal
    .

--- code/PropertyTree.cs
+++ code/PropertyTree.cs
@@ -17,5 +17,26 @@ namespace csMTG
             properties = new Dictionary<string, Dictionary<int, dynamic>>();
         }
 
+        /// <summary>
+        /// Lists all the names of the properties in the dictionary.
+        /// </summary>
+        /// <returns> A list of the properties' names. </returns>
+        public List<string> PropertyNames()
+        {
+            List<string> names = new List<string>(properties.Keys);
+
+            return names;
+        }
+        
+
+        static void Main(String[] args)
+        {
+            PropertyTree prop = new PropertyTree();
+
+            prop.properties.Add("label", null);
+            prop.properties.Add("height", null);
+
+            prop.PropertyNames().ForEach(Console.WriteLine);
+        }
     }
 }
