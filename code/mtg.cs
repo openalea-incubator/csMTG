d26ee632d1074ac5efdfa205324863954ca41658
imane-aanna <aannaque.imane@gmail.com>
2016-05-19 15:22:53 +0200

    Fixes openalea-incubator/csMTG#43
    - Defines the new data structure.
    - Tests the constructor.

---
+++ code/mtg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csMTG
+{
+    public class mtg : PropertyTree
+    {
+
+        #region Attributes
+
+        // Map a vertex to its scale.
+        public Dictionary<int, int> scale;
+
+        // Equivalent of parent. Assigns to each vertex a vertex on scale-1.
+        public Dictionary<int, int> complex;
+
+        // Equivalent of children. Assigns to each vertex a list of vertices on a lower scale (scale+1).
+        public Dictionary<int, List<int>> components;
+
+        #endregion
+
+        /// <summary>
+        /// Constructor of the MTG.
+        /// </summary>
+        public mtg()
+        {
+            // Initialize the attributes
+
+            scale = new Dictionary<int, int>() { { 0, 0 } };
+            complex = new Dictionary<int, int>() { };
+            components = new Dictionary<int, List<int>>() { };
+
+            // Add default properties
+
+            AddProperty("Edge_Type");
+            AddProperty("label");
+
+        }
+
+    }
+}
---
+++ test/MtgTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csMTG;
+using System.Collections.Generic;
+
+namespace csMTG.Tests
+{
+    [TestClass()]
+    public class MtgTests
+    {
+        
+        #region Test of constructor
+        
+        [TestMethod()]
+        public void Mtg_NewMtg_AttributesInitializedPropertiesAdded()
+        {
+            mtg tree = new mtg();
+
+            Assert.AreEqual(tree.scale[0], 0);
+
+            List<string> realList = tree.PropertyNames();
+            List<string> expectedList = new List<string>() { "Edge_Type", "label" };
+
+            CollectionAssert.AreEqual(realList, expectedList);
+
+        }
+
+        #endregion
+
+        
+    }
+}
