9b8d6a1ebcaabfbcb7ef7d46f5543b44ab7a88c0
imane-aanna <aannaque.imane@gmail.com>
2016-06-29 04:33:56 +0200

    Fixes openalea-incubator/csMTG#78
    Added the constructor Gramene which mainly fills two functions:
    * Define the 6 scales of a gramene (canopy, plant, system, axis,
    botanical_unit, organ).
    * Creates an empty canopy.

---
+++ code/Gramene.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csMTG
+{
+    public class Gramene : mtg
+    {
+        // Attributes
+
+        public Dictionary<int, string> labelsOfScales;
+
+        // We may also have attributes that would stand for memory attributes (Plant, stem, last internode)
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor of Gramene:
+        /// * Sets the labels of the 6 scales of an mtg.
+        /// * Creates an mtg composed of one element (canopy).
+        /// </summary>
+        public Gramene()
+        {
+            // define the 6 scales
+
+            labelsOfScales = new Dictionary<int, string>();
+            labelsOfScales.Add(1, "canopy");
+            labelsOfScales.Add(2, "plant");
+            labelsOfScales.Add(3, "system");
+            labelsOfScales.Add(4, "axis");
+            labelsOfScales.Add(5, "botanical_unit");
+            labelsOfScales.Add(6, "organ");
+
+            // add a canopy
+
+            AddComponent(0, componentId: 1);
+
+        }
+
+        #endregion
+
+        // Accessors (g.Plants , g.Stems, ... They will all return a vid or a list of vids)
+
+        // Edition (AddPlant, AddStem,...)
+
+        // Properties (We'll have in the parameters "vid to which the properties will be added + the properties to add")
+
+        // Query functions
+
+
+    }
+}
---
+++ test/GrameneTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csMTG;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csMTG.Tests
+{
+    [TestClass]
+    public class GrameneTests
+    {
+        #region Test of constructor
+
+        [TestMethod]
+        public void Gramene_NewGramene_CanopyAdded()
+        {
+            Gramene g = new Gramene();
+
+            int idOfCanopy = g.labelsOfScales.FirstOrDefault(x => x.Value == "canopy").Key;
+
+            Assert.AreEqual(1, idOfCanopy);
+            Assert.AreEqual("canopy", g.labelsOfScales[idOfCanopy]);
+
+            CollectionAssert.AreEqual(new List<int>() { idOfCanopy }, g.Components(0));
+
+        }
+
+        #endregion
+    }
+}
